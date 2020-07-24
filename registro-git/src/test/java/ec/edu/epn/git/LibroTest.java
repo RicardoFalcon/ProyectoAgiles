843c3251c1fd28c883673fe6403af275c83b25dc
RicardoFalcon <ricardo.falcon@epn.edu.ec>
2020-07-23 20:22:28 -0500

    Primer commit contiene la clase Libro

---
+++ registro-git/src/test/java/ec/edu/epn/git/LibroTest.java
@@ -0,0 +1,14 @@
+package ec.edu.epn.git;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LibroTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
